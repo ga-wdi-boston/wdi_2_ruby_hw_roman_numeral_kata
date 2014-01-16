329f00faf26b1fbc4c013c5b9203fd8cafbade36
emreiser <emilie.reiser@gmail.com>
2014-01-16 18:39:24 -0500

    Scaled tests to evaluate numbers under 10 and passed (with ugly if statement).

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def roman_numerals(number)
-	roman_string = 'I' * number
+
+	if number <= 3
+		roman_string = 'I' * number
+	elsif number == 4
+		roman_string = 'IV'
+	elsif number >= 5 && number < 9
+		roman_string = 'V' + 'I' * (number % 5)
+	else
+		'IX'
+	end
+
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -2,11 +2,14 @@ require 'spec_helper'
 require_relative '../roman_numerals'
 
 describe '#to_roman' do
-	it 'translates an integer under 5 into roman numerals using "I" and "V"' do
+	it 'translates an integer under 10 into roman numerals' do
 		expect(roman_numerals(0)).to eq ''
 		expect(roman_numerals(1)).to eq 'I'
 		expect(roman_numerals(2)).to eq 'II'
 		expect(roman_numerals(3)).to eq 'III'
-
+		expect(roman_numerals(4)).to eq 'IV'
+		expect(roman_numerals(5)).to eq 'V'
+		expect(roman_numerals(8)).to eq 'VIII'
+		expect(roman_numerals(9)).to eq 'IX'
 	end
 end
