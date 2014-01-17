342850c65e2e10b518069f3a0019c4b9ade8b5e7
Prescott Murphy <prurph@gmail.com>
2014-01-16 19:41:46 -0500

    Whoops had some errors in the description of my tests (but not actual results, just misstated the ranges that were covered)

--- spec/numeral_spec.rb
+++ spec/numeral_spec.rb
@@ -14,14 +14,21 @@ describe '#to_roman' do
     expect(to_roman(7)).to eq "VII"
     expect(to_roman(8)).to eq "VIII"
   end
-  it "should return appropriate String of stuff that requires I through X (1-38)" do
+  it "should return appropriate String of stuff that requires I through X (1-39)" do
     expect(to_roman(9)).to eq "IX"
     expect(to_roman(12)).to eq "XII"
     expect(to_roman(18)).to eq "XVIII"
     expect(to_roman(25)).to eq "XXV"
     expect(to_roman(38)).to eq "XXXVIII"
   end
-  it "should return appropriate String of stuff that needs L (1-88)" do
+  it "should return appropriate String of stuff that needs L (1-89)" do
+    expect(to_roman(42)).to eq "XLII"
+    expect(to_roman(58)).to eq "LVIII"
+    expect(to_roman(65)).to eq "LXV"
+    expect(to_roman(79)).to eq "LXXIX"
+    expect(to_roman(88)).to eq "LXXXVIII"
+  end
+  it "should return appropriate String of stuff that needs C (1-399)" do
     expect(to_roman(42)).to eq "XLII"
     expect(to_roman(58)).to eq "LVIII"
     expect(to_roman(65)).to eq "LXV"
