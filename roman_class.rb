0d7eda18e5128560b9b35d6732c138bee7ad9e63
Mike Stone <mikestone14@gmail.com>
2014-01-16 21:35:22 -0500

    Why is RomanNum.to_i always returning 0?

--- roman_class.rb
+++ roman_class.rb
@@ -3,7 +3,8 @@ require 'pry'
 class RomanNum
 	attr_accessor :roman
 	def initialize(value)
-		@roman = value
+		@roman = value.upcase
+		self.to_i
 	end
 
 	def to_i
@@ -11,9 +12,8 @@ class RomanNum
 		normal_chars = [['M', 1000], ['D', 500], ['C', 100],['L', 50], ['X', 10], ['V', 5], ['I', 1]]
 		result = 0
 		counter = 0
-		length = self.roman.length
-		while self.roman.length != 0
-			raise "Invalid entry - not a roman numeral" unless counter <= length
+		while self.roman.length > 0
+			raise "Invalid entry - not a roman numeral" unless counter <= 100
 			weird_chars.each do |group|
 				if self.roman.sub!(group[0], '')
 					result += group[1]
@@ -28,17 +28,14 @@ class RomanNum
 		end
 		result
 	end
-
-	def to_s
-		@roman
-	end
 end
 
-number333 = RomanNum.new("MIKE")
-number2 = RomanNum.new("MIKE")
-integer = number2.to_i
+# number333 = RomanNum.new("XIX").to_i
+# number2 = RomanNum.new("XIX")
+# integer = number2.to_i
 
-puts integer
 
-puts number333
+# puts integer
+
+# puts number333
 
