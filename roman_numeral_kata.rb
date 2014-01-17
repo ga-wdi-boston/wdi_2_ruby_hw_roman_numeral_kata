126f8b3ff305deef2e07a6c7b9fbaf17f9aa5034
Praggya Rustagi Barretto <praggya@gmail.com>
2014-01-16 23:20:09 -0500

    added more cases for spec and conversions on the main rb

--- roman_numeral_kata.rb
+++ roman_numeral_kata.rb
@@ -1,6 +1,6 @@
 class Numbers
 
-	CONVERSION = {1 => "I", 5 => "V"}
+	CONVERSION = {1 => "I", 5 => "V", 10 => "X", 50 => "L", 100 => "C", 500 => "D", 1000 => "M"}
 
 	def initialize(number)
 		@number = number
--- spec/roman_numeral_kata_spec.rb
+++ spec/roman_numeral_kata_spec.rb
@@ -3,11 +3,13 @@ require_relative '../roman_numeral_kata'
 
 describe 'Numbers' do
 
-	[[ 1, "I" ], [ 5, "V" ]].each do |pair|
+	[[ 1, "I" ], [ 5, "V" ], [ 10, "X"], [ 50, "L"], [ 100, "C"], [ 500, "D"], [ 1000, "M"]].each do |pair|
 
 		it "converts #{pair[1]} for #{pair[0]}" do
 			number = Numbers.new(1)
 			expect(number.toRoman).to eq("I")
 		end
+
+
 	end
 end
