a19235d43e6c8bf0ce03b555e5f04df6e56105b9
Jordan Morano <jordan.morano@gmail.com>
2014-01-16 19:01:02 -0500

    pass 4 and 5

--- lib/roman_numeral.rb
+++ lib/roman_numeral.rb
@@ -1,5 +1,17 @@
 
-def to_roman(integer)
-  roman_num_array = %w{I II III IV V VI VII VIII IX X}
-  roman_num_array[0] * integer
+# Symbol  Value
+# I 1
+# V 5
+# X 10
+# L 50
+# C 100
+# D 500
+# M 1,000
+
+
+def to_roman(int)
+  romans = %w{I V X L C D M}
+  return romans[0] * int if int < 4
+  return romans[1] if int % 5 == 0
+  return romans[0] << romans[1] if int % 5 == 4
 end
--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -5,11 +5,16 @@ describe '#to_roman' do
 ### accept a single Integer as an argument and
 ### return a String with the value being the equivalent
   it 'returns a valid roman numeral' do
-    expect(%w{I II III IV V VI VII VIII IX X}).to include to_roman(2)
+    expect(%w{I V X L C D M}).to include to_roman(1)
   end
   it 'returns the correct roman numeral between 1-3' do
     expect(to_roman(2)).to eq 'II'
     expect(to_roman(3)).to eq 'III'
+  end
+
+  it 'returns the correct roman numeral for int close to 5' do
+    expect(to_roman(4)).to eq 'IV'
+    expect(to_roman(5)).to eq 'V'
   end
 end
 
