049f42d8ec49cc75fb56c40db0ae87916e988ee2
Prescott Murphy <prurph@gmail.com>
2014-01-17 08:41:52 -0500

    Refactored to use single conversion array instead of an array of subtractives and an array of regulars.

--- numeral.rb
+++ numeral.rb
@@ -28,34 +28,27 @@ end
 
 def to_arabic(str)
   int = 0
-  subtractives = [
-                  ["CM", 900],
-                  ["CD", 400],
-                  ["XC", 90],
-                  ["XL", 40],
-                  ["IX", 9],
-                  ["IV", 4]
-                  ]
-  normal = [
-            ["M", 1000],
-            ["D", 500],
-            ["C", 100],
-            ["L", 50],
-            ["X", 10],
-            ["V", 5],
-            ["I", 1]
+  conv = [
+          ["M", 1000],
+          ["CM", 900],
+          ["D", 500],
+          ["CD", 400],
+          ["C", 100],
+          ["XC", 90],
+          ["L", 50],
+          ["XL", 40],
+          ["X", 10],
+          ["IX", 9],
+          ["V", 5],
+          ["IV", 4],
+          ["I", 1],
           ]
-
-  loop_count = 0 # Used to determine if invalid string (infinite loop)
+  loop_count = 0
   while str.length > 0
-    subtractives.each.each do |roman, arabic|
-      if str.sub!(/#{roman}/, '')
+    conv.each.each do |roman, arabic|
+      if str.index(roman) == 0
         int += arabic
-      end
-    end
-    normal.each.each do |roman, arabic|
-      if str.sub!(/#{roman}/, '')
-        int += arabic
+        str.slice!(roman)
       end
     end
     loop_count += 1
--- roman_class.rb
+++ roman_class.rb
@@ -6,43 +6,36 @@ class RomanNum
   end
 
   def to_i
-    subtractives = [
-                    ["CM", 900],
-                    ["CD", 400],
-                    ["XC", 90],
-                    ["XL", 40],
-                    ["IX", 9],
-                    ["IV", 4]
-                    ]
-    normal = [
-              ["M", 1000],
-              ["D", 500],
-              ["C", 100],
-              ["L", 50],
-              ["X", 10],
-              ["V", 5],
-              ["I", 1]
-            ]
-
-    loop_count = 0 # Used to determine if invalid string (infinite loop)
     int = 0
+    conv = [
+            ["M", 1000],
+            ["CM", 900],
+            ["D", 500],
+            ["CD", 400],
+            ["C", 100],
+            ["XC", 90],
+            ["L", 50],
+            ["XL", 40],
+            ["X", 10],
+            ["IX", 9],
+            ["V", 5],
+            ["IV", 4],
+            ["I", 1],
+            ]
+    loop_count = 0
     while @roman_string.length > 0
-      subtractives.each.each do |roman, arabic|
-        if @roman_string.sub!(/#{roman}/, '')
+      conv.each.each do |roman, arabic|
+        if @roman_string.index(roman) == 0
           int += arabic
-        end
-      end
-      normal.each.each do |roman, arabic|
-        if @roman_string.sub!(/#{roman}/, '')
-          int += arabic
+          @roman_string.slice!(roman)
         end
       end
       loop_count += 1
-      # If we keep looping the string is invalid
       raise "Not a valid Roman numeral!" if loop_count >= 100
     end
     int
   end
+
   def to_s
     @roman_string
   end
