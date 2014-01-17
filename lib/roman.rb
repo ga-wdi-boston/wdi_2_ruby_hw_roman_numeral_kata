ca86d2496cf813555823ebc84ede72026b43f037
K Mak <karen.mak@stx.oxon.org>
2014-01-16 20:15:08 -0500

    starting over...small steps/tests

--- lib/roman.rb
+++ lib/roman.rb
@@ -1,14 +1,9 @@
-def to_roman(key)
- roman = {1 => 'I',
-          5 => 'V',
-          10 => 'X',
-          50 => 'L',
-          100 => 'C',
-          500 => 'D',
-          1000 => 'M'}  #returns a string
+def to_roman(num)
 
-  return roman[1] + roman[5] if key == 4
-  else roman[key]
+  roman_num = ''
+  while num >= 1
+    roman_num += 'I'
+    num -= 1
   end
-
-
+  roman_num
+end
--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -2,19 +2,17 @@ require 'spec_helper'
 require_relative '../lib/roman'
 
 describe '#to_roman' do
-  it 'takes a base integer (1, 5, 10, 50, 100, 500, 1000) and returns a Roman numeral' do
+  it 'takes base integer (1) and returns a Roman numeral' do
     expect(to_roman (1)).to eq "I"
-    expect(to_roman (5)).to eq "V"
-    expect(to_roman (100)).to eq "C"
-    expect(to_roman (500)).to eq "D"
-    expect(to_roman (1000)).to eq "M"
+
   end
 
-  it 'places an "I" before a "V" when "4" is passed into the method' do
-    expect(to_roman(4)).to eq "IV"
+  it 'tests addition of Roman numerals when input integer is not part of the exception' do
+    expect(to_roman(2)).to eq "II"
+    expect(to_roman(3)).to eq "III"
   end
 end
 
-# I can be placed before V and X to make 4 units (IV) and 9 units (IX)
-# X can be placed before L and C to make 40 (XL) and 90 (XC respectively)
-# C can be placed before D and M to make 400 (CD) and 900 (CM)
+# exception: I can be placed before V and X to make 4 units (IV) and 9 units (IX)
+# exception: X can be placed before L and C to make 40 (XL) and 90 (XC respectively)
+# exception: C can be placed before D and M to make 400 (CD) and 900 (CM)
