918162766f5528d4047bcb50d922abf47879f586
K Mak <karen.mak@stx.oxon.org>
2014-01-16 19:16:38 -0500

    method of base integers passed

--- lib/roman.rb
+++ lib/roman.rb
@@ -1,3 +1,11 @@
-def to_roman(integer)
- 'I'  #returns a string
+def to_roman(key)
+ roman = {1 => 'I',
+          5 => 'V',
+          10 => 'X',
+          50 => 'L',
+          100 => 'C',
+          500 => 'D',
+          1000 => 'M'}  #returns a string
+return roman[key]
 end
+
--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -2,8 +2,16 @@ require 'spec_helper'
 require_relative '../lib/roman'
 
 describe '#to_roman' do
-  it 'takes an integer and returns a Roman numeral' do
+  it 'takes a base integer (1, 5, 10, 50, 100, 500, 1000) and returns a Roman numeral' do
     expect(to_roman (1)).to eq "I"
+    expect(to_roman (5)).to eq "V"
+    expect(to_roman (100)).to eq "C"
+    expect(to_roman (500)).to eq "D"
+    expect(to_roman (1000)).to eq "M"
   end
 end
 
+
+# I can be placed before V and X to make 4 units (IV) and 9 units (IX)
+# X can be placed before L and C to make 40 (XL) and 90 (XC respectively)
+# C can be placed before D and M to make 400 (CD) and 900 (CM)
