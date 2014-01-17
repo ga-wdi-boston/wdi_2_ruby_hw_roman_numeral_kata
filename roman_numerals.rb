d55b1c7d20e7227fa6b7a9a0d6da0c8bf0f46b85
tsyan <anna.tsykalova@gmail.com>
2014-01-17 13:05:56 -0500

    refactored, all methods work, all tests pass

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,11 +1,9 @@
-require 'pry'
-
 def to_roman(*num)
 	raise "Please enter a single integer from 1-3000" 	unless num.length == 1 && num[0].is_a?(Numeric)
 
 	@num = num[0]
 
-	romans = {
+	conversion_table = {
 		1000 => "M",
 		900 => "CM",
     500 => "D",
@@ -23,7 +21,7 @@ def to_roman(*num)
 
 	numbers = []
 
-	romans.each do |k, v|
+	conversion_table.each do |k, v|
 		while @num >= k
 			numbers << v
 			@num = @num - k
@@ -39,71 +37,45 @@ end
 def to_arabic(*string)
 	raise "Please enter a single string!" unless string.length == 1 && string[0].is_a?(String)
 
-	@full_roman = string[0]
+	@roman = string[0]
 
-
-	# binding.pry
-
-	# create array first
 	integer_array = []
 
-	# if there are subtractions, put them into array, and delete them from original array
-	if @full_roman["IV"]
-		integer_array << 4
-		@full_roman.slice!("IV")
-	end
-	if @full_roman["IX"]
-		integer_array << 9
-		@full_roman.slice!("IX")
-	end
-	if @full_roman["XL"]
-		integer_array << 40
-		@full_roman.slice!("XL")
-	end
-	if @full_roman["XC"]
-		integer_array << 90
-		@full_roman.slice!("XC")
-	end
-	if @full_roman["CD"]
-		integer_array << 400
-		@full_roman.slice!("CD")
-	end
-	if @full_roman["CM"]
-		integer_array << 900
-		@full_roman.slice!("CM")
-	end
-
-	# binding.pry
-
-	# at this point:
-	# integer_array should contain any subtractive numbers [4,9,40,90,400,900]
-	# @full_roman should be a string containing the rest of the numerals, e.g. "XXXVII"
-
-	@full_roman_array = @full_roman.split(//)
-	# now i want to convert @full_roman to integers
-
-	# binding.pry
-
-	romans = {
-		1000 => "M",
+	conversion_table_1 = {
 		900 => "CM",
-    500 => "D",
     400 => "CD",
-    100 => "C",
 		90 => "XC",
-		50 => "L",
 		40 => "XL",
-		10 => "X",
 		9 => "IX",
-		5 => "V",
 		4 => "IV",
+	}
+
+	# if there are subtractions, put them into array, and delete them from original array
+	conversion_table_1.each do |integer, roman|
+		if @roman[roman] # if @roman contains "roman"
+			integer_array << integer
+			@roman.slice!(roman)
+		end
+	end
+
+	# now convert the rest of @roman to integers
+
+	# split remaining string into array of characters
+	@roman_array = @roman.split(//)
+
+	conversion_table_2 = {
+		1000 => "M",
+    500 => "D",
+    100 => "C",
+		50 => "L",
+		10 => "X",
+		5 => "V",
 		1 => "I"
 	}
 
-	@full_roman_array.each do |numeral|
-		# find the value in the hash, print corresponding key
-		arabic_number = romans.key(numeral)
-		# append integer to array
+	# convert characters to integers, append to array
+	@roman_array.each do |numeral|
+		arabic_number = conversion_table_2.key(numeral)
 		integer_array << arabic_number
 	end
 
@@ -111,19 +83,57 @@ def to_arabic(*string)
 	integer = integer_array.inject(0) {|sum, i|  sum + i }
 	integer
 
-	# binding.pry
-
 end
 
-binding.pry
+# Laure's answer
 
+# def to_roman(number)
+#   conversion_chart = {
+#     1000 => "M",
+#     900 => "CM",
+#     500 => "D",
+#     400 => "CD",
+#     100 => "C",
+#     90 => "XC",
+#     50 => "L",
+#     40 => "XL",
+#     10 => "X",
+#     9 => "IX",
+#     5 => "V",
+#     4 => "IV",
+#     1 => "I"
+#   }
+#   answer = ''
+#   conversion_chart.keys.each do |divisor|
+#     array = number.divmod(divisor)
+#     answer << conversion_chart[divisor] * array[0]
+#     number = array[1]
+#   end
+#   answer
+# end
 
-
-
-# Laure's answer... my typing is incomplete here
-# answer = 0
-# conversion_chart.each do |key, value|
-# 	while string.index(key).starts_with? == 0
-# 		answer += value
-# 		string.slice!(key)
-# 	end
+# def to_arabic(string)
+#   conversion_chart = {
+#     "M" => 1000,
+#     "CM" => 900,
+#     "D" => 500,
+#     "CD" => 400,
+#     "C" => 100,
+#     "XC" => 90,
+#     "L" => 50,
+#     "XL" => 40,
+#     "X" => 10,
+#     "IX" => 9,
+#     "V" => 5,
+#     "IV" => 4,
+#     "I" => 1
+#   }
+#   answer = 0
+#   for key, value in conversion_chart
+#     while string.index(key) == 0
+#       answer += value
+#       string.slice!(key)
+#     end
+#   end
+#   answer
+# end
