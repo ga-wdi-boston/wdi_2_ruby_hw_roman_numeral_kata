e4e2adc12f4b9733508125c8096a65b17d4fe6b5
Prescott Murphy <prurph@gmail.com>
2014-01-16 19:18:28 -0500

    Tests pass for 1-8 on #to_roman; going to refactor now

--- numeral.rb
+++ numeral.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def to_roman(int)
-  [nil,"I","II","III"][int]
+  answer = ''
+  while int > 0
+    # binding.pry
+    if int >= 5
+      answer += "V"
+      int -= 5
+    elsif int == 4
+      answer += "IV"
+      int -= 4
+    else
+      answer += ("I" * int)
+      int = 0
+    end
+  end
+  answer
 end
--- spec/numeral_spec.rb
+++ spec/numeral_spec.rb
@@ -7,4 +7,11 @@ describe '#to_roman' do
     expect(to_roman(2)).to eq "II"
     expect(to_roman(3)).to eq "III"
   end
+  it "should return appropriate String of only I's and V's for 4-8" do
+    expect(to_roman(4)).to eq "IV"
+    expect(to_roman(5)).to eq "V"
+    expect(to_roman(6)).to eq "VI"
+    expect(to_roman(7)).to eq "VII"
+    expect(to_roman(8)).to eq "VIII"
+  end
 end
