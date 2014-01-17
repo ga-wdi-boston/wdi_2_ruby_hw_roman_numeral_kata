aafb546c372cd611af351dfdfcfd71424010af14
Praggya Rustagi Barretto <praggya@gmail.com>
2014-01-17 02:14:41 -0500

    examples of 1 for 1 translation between roman and arabic tests pasts

--- arabic_numeral_kata.rb
+++ arabic_numeral_kata.rb
@@ -1,2 +1,13 @@
-def toArabic(number)
+def toArabic(input)
+
+	conversion = {"M"=>1000,"CM"=>900,"D"=>500,"CD"=>400,"C"=>100,"XC"=>90,"L"=>50, "XL"=>40,"X"=>10, "IX"=>9, "V"=>5, "IV"=>4, "I"=>1 }
+
+	ret = 0
+
+	conversion.keys.each do |value|
+		if input.include?(value)
+			ret += conversion[value]
+		end
+	end
+	ret
 end
--- spec/arabic_numeral_kata_spec.rb
+++ spec/arabic_numeral_kata_spec.rb
@@ -2,7 +2,10 @@ require 'spec_helper'
 require_relative '../arabic_numeral_kata'
 
 describe '#toArabic' do
-		it "should return nil for empty string" do
-     expect(toArabic('')).to eq nil
-  end
+  	it "should return 1 for I" do
+  		expect(toArabic("I")).to eq 1
+  	end
+  	it "should result 10 for X" do
+  		expect(toArabic("X")).to eq 10
+  	end
 end
