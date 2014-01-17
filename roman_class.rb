3b4f4196c0b69e6d4584ce0e5f094ae2fab4af1c
Mike Stone <mikestone14@gmail.com>
2014-01-16 21:07:30 -0500

    Raise an error when to_i is called on an invalid numeral

--- roman_class.rb
+++ roman_class.rb
@@ -1,18 +1,21 @@
 require 'pry'
 
-class RomanNum < String
+class RomanNum
 	attr_accessor :roman
 	def initialize(value)
 		@roman = value
 	end
 
 	def to_i
-		weird_chars = [['iv', 4], ['ix', 9], ['xc', 90], ['cd', 400], ['cm', 900]]
-		normal_chars = [['m', 1000], ['d', 500], ['c', 100],['l', 50], ['x', 10], ['v', 5], ['i', 1]]
+		weird_chars = [['IV', 4], ['IX', 9], ['XC', 90], ['CD', 400], ['CM', 900]]
+		normal_chars = [['M', 1000], ['D', 500], ['C', 100],['L', 50], ['X', 10], ['V', 5], ['I', 1]]
 		result = 0
+		counter = 0
+		length = self.roman.length
 		while self.roman.length != 0
+			raise "Invalid entry - not a roman numeral" unless counter <= length
 			weird_chars.each do |group|
-				if self.sub!(group[0], '')
+				if self.roman.sub!(group[0], '')
 					result += group[1]
 				end
 			end
@@ -21,9 +24,21 @@ class RomanNum < String
 					result += group[1]
 				end
 			end
+			counter += 1
 		end
 		result
 	end
+
+	def to_s
+		@roman
+	end
 end
 
+number333 = RomanNum.new("MIKE")
+number2 = RomanNum.new("MIKE")
+integer = number2.to_i
+
+puts integer
+
+puts number333
 
