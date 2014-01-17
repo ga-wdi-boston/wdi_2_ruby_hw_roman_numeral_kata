afef780a2a4f4eaf4a403b768dd2ef5311add76f
Jordan Morano <jordan.morano@gmail.com>
2014-01-16 22:21:33 -0500

    Moved #to_roman to RomanNum class

--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -1,59 +1,50 @@
 require 'spec_helper'
 require_relative '../lib/roman_numeral'
 
-describe '#to_roman' do
-### accept a single Integer as an argument and
-### return a String with the value being the equivalent
-  it 'returns a valid roman numeral' do
-    expect(%w{I V X L C D M}).to include to_roman(1)
+describe RomanNum do
+  describe '#to_roman' do
+
+    it 'returns the correct roman numeral between 1-3' do
+      expect(RomanNum.new(2).roman).to eq 'II'
+      expect(RomanNum.new(3).roman).to eq 'III'
+    end
+
+    it 'returns the correct roman numeral for int close to 5' do
+      expect(RomanNum.new(4).roman).to eq 'IV'
+      expect(RomanNum.new(5).roman).to eq 'V'
+    end
+
+    it 'returns the correct roman numeral for int btwn 5 10' do
+      expect(RomanNum.new(6).roman).to eq 'VI'
+      expect(RomanNum.new(8).roman).to eq 'VIII'
+      expect(RomanNum.new(9).roman).to eq 'IX'
+      expect(RomanNum.new(10).roman).to eq 'X'
+    end
+
+    it 'returns the correct roman numeral for int btwn 11, 48' do
+      expect(RomanNum.new(11).roman).to eq 'XI'
+      expect(RomanNum.new(14).roman).to eq 'XIV'
+      expect(RomanNum.new(29).roman).to eq 'XXIX'
+      expect(RomanNum.new(35).roman).to eq 'XXXV'
+    end
+
+    it 'returns the correct roman numeral for int btwn 49, 101' do
+      expect(RomanNum.new(49).roman).to eq 'XLIX'
+      expect(RomanNum.new(50).roman).to eq 'L'
+      expect(RomanNum.new(91).roman).to eq 'XCI'
+      expect(RomanNum.new(101).roman).to eq 'CI'
+    end
+
+    it 'returns the correct roman numeral for high int!!' do
+      expect(RomanNum.new(424).roman).to eq 'CDXXIV'
+      expect(RomanNum.new(591).roman).to eq 'DXCI'
+      expect(RomanNum.new(909).roman).to eq 'CMIX'
+      expect(RomanNum.new(3494).roman).to eq 'MMMCDXCIV'
+      expect(RomanNum.new(4999).roman).to eq 'MMMMCMXCIX'
+    end
   end
-  it 'returns the correct roman numeral between 1-3' do
-    expect(to_roman(2)).to eq 'II'
-    expect(to_roman(3)).to eq 'III'
+
+  describe '#to_arabic' do
+
   end
-
-  it 'returns the correct roman numeral for int close to 5' do
-    expect(to_roman(4)).to eq 'IV'
-    expect(to_roman(5)).to eq 'V'
-  end
-
-  it 'returns the correct roman numeral for int btwn 5 10' do
-    expect(to_roman(6)).to eq 'VI'
-    expect(to_roman(8)).to eq 'VIII'
-    expect(to_roman(9)).to eq 'IX'
-    expect(to_roman(10)).to eq 'X'
-  end
-
-  it 'returns the correct roman numeral for int btwn 11, 48' do
-    expect(to_roman(11)).to eq 'XI'
-    expect(to_roman(14)).to eq 'XIV'
-    expect(to_roman(29)).to eq 'XXIX'
-    expect(to_roman(35)).to eq 'XXXV'
-  end
-
-  it 'returns the correct roman numeral for int btwn 49, 101' do
-    expect(to_roman(49)).to eq 'XLIX'
-    expect(to_roman(50)).to eq 'L'
-    expect(to_roman(91)).to eq 'XCI'
-    expect(to_roman(101)).to eq 'CI'
-  end
-
-  it 'returns the correct roman numeral for high int!!' do
-    expect(to_roman(424)).to eq 'CDXXIV'
-    expect(to_roman(591)).to eq 'DXCI'
-    expect(to_roman(909)).to eq 'CMIX'
-    expect(to_roman(3494)).to eq 'MMMCDXCIV'
-    expect(to_roman(4999)).to eq 'MMMMCMXCIX'
-  end
-end
-
-
-
-
-
-
-
-
-describe '#to_arabic' do
-
 end
