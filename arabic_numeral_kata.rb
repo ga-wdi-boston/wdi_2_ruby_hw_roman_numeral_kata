c6b66892a5a40b83a0229fa18f1269193c9998d2
Praggya Rustagi Barretto <praggya@gmail.com>
2014-01-17 02:28:07 -0500

    added additional examples for toArabic

--- arabic_numeral_kata.rb
+++ arabic_numeral_kata.rb
@@ -2,12 +2,13 @@ def toArabic(input)
 
 	conversion = {"M"=>1000,"CM"=>900,"D"=>500,"CD"=>400,"C"=>100,"XC"=>90,"L"=>50, "XL"=>40,"X"=>10, "IX"=>9, "V"=>5, "IV"=>4, "I"=>1 }
 
-	ret = 0
+	numeral = 0
 
-	conversion.keys.each do |value|
-		if input.include?(value)
-			ret += conversion[value]
+	for key, value in conversion
+		while input.index(key) == 0
+			numeral += value
+			input.slice!(key)
 		end
 	end
-	ret
+	numeral
 end
--- spec/arabic_numeral_kata_spec.rb
+++ spec/arabic_numeral_kata_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 require_relative '../arabic_numeral_kata'
 
 describe '#toArabic' do
-  	it "should return 1 for I" do
-  		expect(toArabic("I")).to eq 1
-  	end
-  	it "should result 10 for X" do
-  		expect(toArabic("X")).to eq 10
-  	end
+ 	it "should return 1 for I" do
+  	expect(toArabic("I")).to eq 1
+  end
+  it "should result 10 for X" do
+  	expect(toArabic("X")).to eq 10
+  end
+  it "should return 300 for CCC" do
+  	expect(toArabic("CCC")).to eq 300
+  end
+  it "should return 543 for DXLIII" do
+  	expect(toArabic("DXLIII")).to eq 543
+  end
+
 end
