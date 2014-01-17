79f77ca1d84a9bd179c7a536a806c1a33c527192
Yiyang Song <songyiyang2012@gmail.com>
2014-01-16 20:30:43 -0500

    Yiyang 1st

--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -5,5 +5,11 @@ describe '#to_roman' do
 	it 'will return the roman number I of integer 1' do
 		expect(to_roman(1)).to eq 'I'
 	end
-
+	it 'will return the roman number of II if interger 2' do
+		expect(to_roman(2)).to eq 'II'
+	end
+	it 'will return "IV" if pass 4' do
+		expect(to_roman(4)).to eq 'IV'
+	end
+	
 end
