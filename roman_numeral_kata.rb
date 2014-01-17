38bde9013426692d3a5f60a3fb2ca2f3f5483121
Praggya Rustagi Barretto <praggya@gmail.com>
2014-01-17 01:03:31 -0500

    Take 2 starting back from the beginning

--- roman_numeral_kata.rb
+++ roman_numeral_kata.rb
@@ -1,16 +1,3 @@
-class Numbers
-
-	CONVERSION = {1 => "I", 5 => "V", 10 => "X", 50 => "L", 100 => "C", 500 => "D", 1000 => "M"}
-
-	def initialize(number)
-		@number = number
-	end
-
-	def toRoman
-		if CONVERSION[@number]
-			return CONVERSION[@number]
-		else
-			return "I" * @number
-		end
-	end
+def convert(value)
+  return "" if value.zero?
 end
--- spec/roman_numeral_kata_spec.rb
+++ spec/roman_numeral_kata_spec.rb
@@ -1,15 +1,11 @@
 require 'spec_helper'
 require_relative '../roman_numeral_kata'
 
-describe 'Numbers' do
+  describe "converting arabic to roman numerals" do
+  	describe "romans didn't have 0" do
+    	it "converts 0 to a blank string" do
+      	expect(convert(0)).to eq("")
+    	end
+  	end
+  end
 
-	[[ 1, "I" ], [ 5, "V" ], [ 10, "X"], [ 50, "L"], [ 100, "C"], [ 500, "D"], [ 1000, "M"]].each do |pair|
-
-		it "converts #{pair[1]} for #{pair[0]}" do
-			number = Numbers.new(1)
-			expect(number.toRoman).to eq("I")
-		end
-
-
-	end
-end
