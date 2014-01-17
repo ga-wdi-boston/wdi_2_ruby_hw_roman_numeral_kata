a094129352560e2227d28908b35e6fc208740ef9
Jack Henley <jack.d.henley@gmail.com>
2014-01-16 23:20:33 -0500

    1-9 working

--- roman.rb
+++ roman.rb
@@ -1,11 +1,31 @@
-def to_roman(num)
+require 'pry'
+
+numerals = {
+        5 => "V",
+        4 => "IV",
+        1 => "I"
+}
+
+def to_roman(num, numerals)
 	roman = []
-	if num == 5
-		roman << 'V'
-	elsif num == 4
-		roman << 'IV'
-	else
-		roman << ('I' * num)
+	numerals.each do | numeral , value |
+		roman << value * (num / numeral)
+		num = num % numeral
 	end
 	roman.join
 end
+
+
+# 	end
+# 	if num >= 5
+# 		num = (num % 5)
+# 		roman << 'V'
+# 	elsif num == 4
+# 		roman << 'IV'
+
+# 	elsif num < 4
+# 		roman << ('I' * num)
+# 		binding.pry
+# 	end
+# 	puts roman.join
+# end
--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -3,18 +3,28 @@ require_relative '../roman.rb'
 
 describe '#to_roman' do
 
+	numerals = {
+        5 => "V",
+        4 => "IV",
+        1 => "I"
+}
+
 	it 'should return i when passed 1' do
-		expect(to_roman(1)).to eq 'I'
+		expect(to_roman(1, numerals)).to eq 'I'
 	end
 
 	it 'should return ii when passed 2' do
-		expect(to_roman(2)).to eq 'II'
+		expect(to_roman(2, numerals)).to eq 'II'
 	end
 
 	it 'should return IV when passed 4' do
-		expect(to_roman(4)).to eq 'IV'
+		expect(to_roman(4, numerals)).to eq 'IV'
 	end
 	it 'should return V when passed 5' do
-		expect(to_roman(5)).to eq 'V'
+		expect(to_roman(5, numerals)).to eq 'V'
+	end
+	it 'should return correct when passed 6-8' do
+		expect(to_roman(6, numerals)).to eq 'VI'
+		expect(to_roman(8, numerals)).to eq 'VIII'
 	end
 end
