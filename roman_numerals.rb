94788f0feb3a2070576eb6bf9fafef5693942a2e
tsyan <anna.tsykalova@gmail.com>
2014-01-17 06:35:50 -0500

    updated argument for to_roman

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,3 +1,7 @@
-def to_roman(num)
-	raise "Please enter a single integer from 1-2000" 	unless num.is_a?(Numeric) && num.between?(1,2000)
+require 'pry'
+
+def to_roman(*num)
+	raise "Please enter a single integer from 1-2000" 	unless num.length == 1 && num[0].is_a?(Numeric)
 end
+
+
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -4,6 +4,7 @@ describe '#to_roman' do
 
 	it 'raises an error if the argument is not a single integer' do
 		expect{ to_roman("string") }.to raise_error "Please enter a single integer from 1-2000"
+		expect{ to_roman(1,2) }.to raise_error "Please enter a single integer from 1-2000"
 	end
 
 	# it
